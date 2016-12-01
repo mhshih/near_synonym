d4bafc2f455d7b64dbfed5467c6fca8ff5d9e3cb
mhshih <d00142002@ntu.edu.tw>
2016-12-01 17:13:38 +0800

    od[r+len(ds)]=ds then od.pop(r)

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Python 2.7
 Django 1.10
 
 - git clone http://github.com/mhshih/near_synonym
-- python2 makemigrations near_synonym
-- python2 migrate
+- python2 manage.py makemigrations near_synonym
+- python2 manage.py migrate
 - python2 manage.py shell
 - import shell
 - python2 manage.py runserver 0.0.0.0:8000
--- shell.py
+++ shell.py
@@ -9,7 +9,6 @@ def append_to_grds(governor,relation,dependent,sentence,grds):
     if governor not in grds:grds[governor]={relation:{dependent:[sentence]}}
     elif relation not in grds[governor]:grds[governor][relation]={dependent:[sentence]}
     elif dependent not in grds[governor][relation]:grds[governor][relation][dependent]=[sentence]
-#   else:grds[governor][relation][dependent].append(sentence)
     elif sentence not in grds[governor][relation][dependent]:
         grds[governor][relation][dependent].append(sentence)
 
@@ -20,7 +19,7 @@ def attach_ch_rel(REL):
 
 FORM_HEAD_DEPREL=[('ROOT',0,'root')] #[{FORM,HEAD,DEPREL},...]
 words=[]
-for line in open('zh-ud-train.conllu'):
+for line in open('parsed'):
     if line=='\n':
         for FORM,HEAD,DEPREL in FORM_HEAD_DEPREL:
             governor=FORM_HEAD_DEPREL[HEAD][0]
@@ -35,8 +34,8 @@ for line in open('zh-ud-train.conllu'):
 
 for governor,rds in grds.items():
     print(governor)
-    rds=OrderedDict(sorted(rds.items(),key=lambda ds:sum([len(ds[1][d]) for d in ds[1]])))#,reverse=True))
+    rds=OrderedDict(sorted(rds.items(),key=lambda r_ds:sum([len(r_ds[1][d]) for d in r_ds[1]]),reverse=True))
     for r,ds in rds.items():
-        rds[r]=OrderedDict(sorted(ds.items(),key=lambda s:len(s),reverse=True))
+        rds[r]=OrderedDict(sorted(ds.items(),key=lambda d_s:len(d_s[1]),reverse=True))
     GRDS(governor=governor,rds=dumps(rds,ensure_ascii=False)).save()
 

aa578c8201fdfcab3d0e29828f3a6511bcc8247f
mhshih <d00142002@ntu.edu.tw>
2016-12-05 18:09:21 +0800

    sorted(g_sents.items(),key=lambda g_sents:len(g_sents[1])) then sorted(nltk.cwn.all_lemma_names(),key=lambda lemma:len(synsets(lemma)))

--- shell.py
+++ shell.py
@@ -1,13 +1,13 @@
 #-*-coding:utf-8-*-
-from near_synonym.models import GRDS
+#from near_synonym.models import GRDS
 from json import dumps
 from collections import OrderedDict
 
 grds={} #g_rds[governor]={relation:{dependent:sentences}}
 
 def append_to_grds(governor,relation,dependent,sentence,grds):
-    if governor not in grds:grds[governor]={relation:{dependent:[sentence]}}
-    elif relation not in grds[governor]:grds[governor][relation]={dependent:[sentence]}
+    if governor not in grds:grds[governor]=OrderedDict({relation:OrderedDict({dependent:[sentence]})})
+    elif relation not in grds[governor]:grds[governor][relation]=OrderedDict({dependent:[sentence]})
     elif dependent not in grds[governor][relation]:grds[governor][relation][dependent]=[sentence]
     elif sentence not in grds[governor][relation][dependent]:
         grds[governor][relation][dependent].append(sentence)
@@ -32,10 +32,22 @@ for line in open('parsed'):
     FORM_HEAD_DEPREL.append((FORM,int(HEAD),DEPREL))
     words.append(FORM)
 
+g_sents=dict()
 for governor,rds in grds.items():
-    print(governor)
+    g_sents[governor]=set()
+#   print(governor)
     rds=OrderedDict(sorted(rds.items(),key=lambda r_ds:sum([len(r_ds[1][d]) for d in r_ds[1]]),reverse=True))
     for r,ds in rds.items():
         rds[r]=OrderedDict(sorted(ds.items(),key=lambda d_s:len(d_s[1]),reverse=True))
+        for d,s in ds.items():
+            g_sents[governor]=g_sents[governor].union(set(s))
+    grds[governor]=rds
     GRDS(governor=governor,rds=dumps(rds,ensure_ascii=False)).save()
-
+for g,sents in sorted(g_sents.items(),key=lambda g_sents:len(g_sents[1]),reverse=True):
+#   print(g,sents)
+    print(g,len(sents))#grds[g]))#,grds[g])
+#   grds[g]=grds[g]
+    rds=grds[g]
+    grds.pop(g)
+    grds[g]=rds
+#for g,rds in grds.items():print(g,rds)
